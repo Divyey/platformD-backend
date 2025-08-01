14528cfd9fa719a9798de1d5f3251fb8a97c5fbf
Divyey Arora <95075707+Divyey@users.noreply.github.com>
2025-08-01 12:27:48 +0530

    updated CORS Middleware in main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.api.v1 import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="PlatformD Backend API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:5173"],  # frontend dev URL(s)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api/v1")
 
